e27fad01d5808db36986cbb2fb5221168a44d497
Betul KAYA <55402564+bbetulkaya@users.noreply.github.com>
2023-07-31 12:48:14 +0300

    Feat: add new way to get user input

--- 02_JS/02_UserInput/index.js
+++ 02_JS/02_UserInput/index.js
@@ -2,3 +2,11 @@
 // let username = window.prompt("What's your name?");
 // console.log(username);
 
+//* DIFFICULT WAY HTML textbox
+
+let username;
+document.getElementById("myButton").onclick = function(){
+    username = document.getElementById("myText").value;
+    console.log(username);
+    document.getElementById("myLabel").innerHTML = "Hello " + username;
+}
